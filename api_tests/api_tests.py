e25163dcfc1a4fad695685e2d1654078f0ee4ffc
Sudhir Jonathan <sudhir.j@gmail.com>
2011-11-28 19:19:29 +0530

    added image tests

--- api_tests/api_tests.py
+++ api_tests/api_tests.py
@@ -2,23 +2,42 @@ import unittest, logging
 import requests, json
 
 def join(*args):
-    return '/'.join([arg.lstrip('/').rstrip('/') for arg in args])
+    return '/'.join([arg.strip('/') for arg in args])
 
 class JsonServer:
     def __init__(self, host, *args, **kwargs):
         self._host = host
     
-    def get(self, path):
+    def get(self, path, *args, **kwargs):
         url = join(self._host, path)
-        response = requests.get(url)
-        logging.info('Hitting %s' % url)
+        response = requests.get(url, *args, **kwargs)
+        response.raise_for_status()
+        logging.info('GETting %s' % url)
         data = json.loads(response.content)
         logging.info('Recieved: %s' % repr(data))
         return data        
     
-    def post(self, path, data):
-        pass        
+    def post(self, path, data, *args, **kwargs):
+        url = join(self._host, path)
+        logging.info('POSTing to %s' % url)
+        response = requests.post(url, data = data, *args, **kwargs)        
+        response.raise_for_status()
+        logging.info('Recieved: %s' % repr(response))
 
+class SignsServer(JsonServer):
+    def __init__(self, client_id = None, 
+                        secret_key = None,
+                        host = 'http://localhost:8080/api/v1/',
+                        *args, **kwargs):
+        self._client_id = client_id
+        self._secret_key = secret_key
+        JsonServer.__init__(self, host, *args, **kwargs)
+        
+    def get_images(self):
+        return self.get('/images')
+        
+    def upload(self, file):
+        self.post('/upload', data=dict(test = 'hello there'), files={'image':file})
 
 class SignsTestServer(JsonServer):
     def create_test_account(self):
@@ -27,7 +46,14 @@ class SignsTestServer(JsonServer):
 
 class ApiTest(unittest.TestCase):
     def test_base_api(self):
-        server = SignsTestServer('http://localhost:8080')
-        account = server.create_test_account()
+        test_server = SignsTestServer('http://localhost:8080')
+        account = test_server.create_test_account()
+        server = SignsServer(client_id = account['client_id'], 
+                            secret_key = account['secret_key'])
+        self.assertListEqual([], server.get_images())
+        server.upload(open('api_tests/test2.jpg', 'rb'))
+        self.assertEqual(1, len(server.get_images()))
+        
+        
         
         
--- app/main.py
+++ app/main.py
@@ -1,13 +1,43 @@
 
 from google.appengine.ext import webapp
+from webapp2_extras import routes
+import json, logging
 
 debug = True
+
+class JsonHandler(webapp.RequestHandler):
+    def send(self, data):
+        self.response.out.write(json.dumps(data))
+
+class ImagesHandler(JsonHandler):
+    def get(self):
+        self.send([])
+
+class UploadHandler(JsonHandler):
+    def post(self):
+        logging.info(len(self.request.get('image')))
+        logging.info(len(self.request.get('test')))
+        self.send([])
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
         self.response.out.write('Hello world!')
 
-ROUTES = [('/', MainHandler)]
+
+def route_with_prefix(prefix, route_defs):
+    return routes.PathPrefixRoute(prefix, 
+        [webapp.Route(route[0], route[1]) for route in route_defs]
+    )
+
+API_V1 = route_with_prefix('/api/v1',[
+    ('/images', ImagesHandler),
+    ('/upload', UploadHandler)
+])
+
+ROUTES = [
+    API_V1,
+    ('/', MainHandler)
+]
 
 if debug:
     import testing_handlers
