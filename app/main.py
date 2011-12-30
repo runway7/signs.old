fc8e50c94694ea57d908e6ffed93d03b7a809b57
Sudhir Jonathan <sudhir.j@gmail.com>
2011-12-30 23:52:03 +0530

    added NDB top-level

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from google.appengine.ext import webapp
+from google.appengine.ext.ndb import context
 import routes
 
 debug = True
@@ -9,6 +10,7 @@ if debug:
     import testing_handlers
     ROUTES.insert(0, ('/testing/create', testing_handlers.CreateHandler))
     
-application = webapp.WSGIApplication(ROUTES, debug = debug)
+application = context.toplevel(webapp.WSGIApplication(ROUTES, debug = debug).__call__)
+
 
 
